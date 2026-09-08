da11f501412e985a0079d00718488d6358846c80
2hats <vaishnavajay@2hatslogic.com>
2026-09-08 18:10:05 +0530

    now it shows right and wrong answers

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,64 +3,79 @@ let currentIndex = 0;
 let score = 0;
 
 function startQuiz() {
-    currentIndex = 0;
-    score = 0;
-    document.getElementById("quizBox").innerHTML = "Loading questions...";
+  currentIndex = 0;
+  score = 0;
+  document.getElementById("quizBox").innerHTML = "Loading questions...";
 
-    fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            questions = data.results;
-            showQuestions();
-        })
-        .catch(function (error) {
-            document.getElementById("quizBox").innerHTML = "Failed to load questions.";
-        });
-}
-
-function showQuestions() {
-    if (currentIndex >= questions.length) {
-        document.getElementById("quizBox").innerHTML = "<p>Quiz finished!</p><p>Your score: " + score + "/" + questions.length + "</p>";
-        return;
-    }
-
-    const question = questions[currentIndex];
-
-    let answers = question.incorrect_answers.slice();
-    answers.push(question.correct_answer);
-    answers = shuffle(answers);
-
-    let html = "<p>" + decodeHTML(question.question) + "</p>";
-
-    answers.forEach(function (answer) {
-        html += '<button onclick="checkAnswer(\'' + encodeURIComponent(answer) + '\')">' + decodeHTML(answer) + "</button><br>";
+  fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      questions = data.results;
+      showQuestion();
+    })
+    .catch(function (error) {
+      document.getElementById("quizBox").innerHTML = "Failed to load questions.";
     });
-
-    document.getElementById("quizBox").innerHTML = html;
 }
 
-function checkAnswer(encodedAnswer) {
-    const answer = decodeURIComponent(encodedAnswer);
-    const correct = questions[currentIndex].correct_answer;
+function showQuestion() {
+  if (currentIndex >= questions.length) {
+    document.getElementById("quizBox").innerHTML =
+      "<p>Quiz finished!</p><p>Your score: " + score + " / " + questions.length + "</p>";
+    return;
+  }
 
-    if (answer === correct) {
-        score++;
+  const question = questions[currentIndex];
+
+  // combine correct and incorrect answers, then shuffle
+  let answers = question.incorrect_answers.slice();
+  answers.push(question.correct_answer);
+  answers = shuffle(answers);
+
+  let html = "<p>" + decodeHTML(question.question) + "</p>";
+
+  answers.forEach(function (answer) {
+    html += '<button class="answerBtn" data-answer="' + encodeURIComponent(answer) + '" onclick="checkAnswer(this)">' + decodeHTML(answer) + "</button><br>";
+  });
+
+  document.getElementById("quizBox").innerHTML = html;
+}
+
+function checkAnswer(clickedButton) {
+  const answer = decodeURIComponent(clickedButton.getAttribute("data-answer"));
+  const correct = questions[currentIndex].correct_answer;
+  const allButtons = document.querySelectorAll(".answerBtn");
+
+  allButtons.forEach(function (button) {
+    button.disabled = true;
+    const buttonAnswer = decodeURIComponent(button.getAttribute("data-answer"));
+
+    if (buttonAnswer === correct) {
+      button.classList.add("correct");
+    } else if (button === clickedButton) {
+      button.classList.add("wrong");
     }
+  });
 
-    currentIndex++;
-    showQuestions();
+  if (answer === correct) {
+    score++;
+  }
+
+  currentIndex++;
+
+  setTimeout(showQuestion, 1200);
 }
 
 function shuffle(array) {
-    return array.sort(function () {
-        return Math.random() - 0.5;
-    });
+  return array.sort(function () {
+    return Math.random() - 0.5;
+  });
 }
 
 function decodeHTML(text) {
-    const textarea = document.createElement("textarea");
-    textarea.innerHTML = text;
-    return textarea.value;
+  const textarea = document.createElement("textarea");
+  textarea.innerHTML = text;
+  return textarea.value;
 }
